8774ab7e93f8443ae87a37181ebad2eb662eb33b
DhereSantosh <dheresantosh9@gmail.com>
2019-06-11 18:41:52 +0530

    First Commit-Review for EmployeeDTO class

---
+++ src/main/java/com/onlyfullstack/springasyncexample/service/EmployeeServiceImpl.java
@@ -0,0 +1,41 @@
+package com.onlyfullstack.springasyncexample.service;
+
+import com.onlyfullstack.springasyncexample.datatransferobject.EmployeeDTO;
+import com.onlyfullstack.springasyncexample.domainobject.EmployeeDO;
+import com.onlyfullstack.springasyncexample.exception.EntityNotFoundException;
+import com.onlyfullstack.springasyncexample.repositoy.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService{
+
+	@Autowired
+	private EmployeeRepository repository;
+	
+	@Override
+	public void addEmployee(EmployeeDO employee) {
+		repository.save(employee);
+	}
+
+	@Override
+	public List<EmployeeDO> getAllEmployees() {
+		return (List<EmployeeDO>) repository.findAll();
+	}
+	
+	@Override
+	public EmployeeDO getEmployee(Long employeeId) throws EntityNotFoundException {
+		return repository.findById(employeeId).orElseThrow(() 
+				-> new EntityNotFoundException("Employee", "empId", employeeId.toString()));
+	}
+
+	@Override
+	public void deleteEmployee(Long employeeId) throws EntityNotFoundException {
+		EmployeeDO employee = repository.findById(employeeId).orElseThrow(() 
+				-> new EntityNotFoundException("Employee", "empId", employeeId.toString()));
+		repository.delete(employee);
+	}
+}
